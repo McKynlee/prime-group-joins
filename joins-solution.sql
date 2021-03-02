86235b474b75a18f594f091e342c854bcda1c244
McKynlee Westman <mckynlee.westman@gmail.com>
2021-03-02 13:25:06 -0600

    Joining orders and line_item

--- joins-solution.sql
+++ joins-solution.sql
@@ -3,7 +3,8 @@ SELECT "customers".first_name, "customers".last_name, "addresses".street, "addre
 JOIN "addresses" ON "customers".id = "addresses".customer_id;
 
 -- 2. Get all orders and their line items (orders, quantity and product).
-
+SELECT "orders".order_date, "line_items".quantity, "line_items".product_id FROM "orders"
+JOIN "line_items" ON "orders".id = "line_items".order_id;
 
 -- 3. Which warehouses have cheetos?
 
